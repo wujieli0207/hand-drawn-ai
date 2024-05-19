3a7066591c97f54b47551568f70cb7098a4ce748
wujieli0207 <wujieli0207@outlook.com>
2024-05-19 21:37:34 +0800

    chore: 引入 vercel 和 google 统计

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
+import GoogleAnalytics from './GoogleAnalytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +19,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        {process.env.NODE_ENV === 'development' ? (
+          <></>
+        ) : (
+          <>
+            <Analytics />
+            <GoogleAnalytics />
+          </>
+        )}
+      </body>
     </html>
   )
 }
