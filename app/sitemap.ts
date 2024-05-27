b59e47fd685217fa05adf3d8142334f7d5a3837a
wujieli0207 <wujieli0207@outlook.com>
2024-05-27 22:28:19 +0800

    fix: sitemap 支持多语言路由

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,5 @@
 import { siteConfig } from '@/config/site'
+import { locales } from '@/i18n'
 import { fetchImages } from '@/lib/fetch/gallery'
 import { IImage } from '@/types/gallery'
 import { MetadataRoute } from 'next'
@@ -9,10 +10,15 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const result = await fetchImages()
   const images = result as IImage[]
 
-  const imageRoutes = images.map((item) => ({
-    url: `${siteUrl}/${item.id}`,
-    lastModified: item.created_at,
-  }))
+  const imageRoutes = images.flatMap((item) => {
+    return locales.map((locale) => {
+      const lang = locale === 'en' ? '' : `/${locale}`
+      return {
+        url: `${siteUrl}${lang}/image/${item.id}`,
+        lastModified: item.created_at,
+      }
+    })
+  })
 
   const routes = [''].map((route) => ({
     url: `${siteUrl}/${route}`,
