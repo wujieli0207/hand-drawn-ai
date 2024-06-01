4c3f57c5c74248fbd3dfa656197ca8cde9dee615
wujieli0207 <wujieli0207@outlook.com>
2024-06-01 17:26:36 +0800

    fix: robots 中的 sitemap 路径错误

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: `${siteUrl}/sitemap.xml`,
+    sitemap: `${siteUrl}sitemap.xml`,
     host: siteUrl,
   }
 }
