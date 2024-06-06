9e5580a29badda9c5162f97132064f352025ab69
wujieli0207 <wujieli0207@outlook.com>
2024-06-06 15:11:46 +0800

    feat: 分类详情增加 metadata

--- app/[locale]/(with-footer)/category/[slug]/page.tsx
+++ app/[locale]/(with-footer)/category/[slug]/page.tsx
@@ -3,6 +3,35 @@ import { createClient } from '@/lib/supabase/server'
 import { ICategory } from '@/types/gallery'
 import { fetchImages } from '@/lib/fetch/gallery'
 import GalleryImages from '@/components/Gallery/GalleryImages'
+import { Metadata } from 'next'
+import { siteConfig, siteName } from '@/config/site'
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string }
+}): Promise<Metadata> {
+  const { slug } = params
+
+  const supabase = createClient()
+
+  const { data: allCategoryData } = await supabase.from('category').select()
+  const allCategory = allCategoryData as ICategory[]
+
+  const currentCategory = allCategory.find(
+    (category) => category.secondCategory === slug
+  )!
+
+  return {
+    title: `${siteName} - Category: ${currentCategory.firstCategory} - ${currentCategory.secondCategory}`,
+    description: `${currentCategory.secondCategory} hand drawn art`,
+    keywords: [
+      ...siteConfig.keywords,
+      currentCategory.firstCategory,
+      currentCategory.secondCategory,
+    ].join(','),
+  }
+}
 
 export default async function CategoryPage({
   params,
